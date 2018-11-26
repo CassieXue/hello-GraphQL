6a5bf611f07f0b0293a493220a28c379fb71df28
hfpp2012 <hfpp2012@gmail.com>
2018-11-26 16:45:28 +0800

    05_Root_Query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraqhQLString } = require('graphql');
+const { GraphQLObjectType, GraqhQLString, GraqhQLSchema } = require('graphql');
 
 const BookType = GraphQLObjectType({
   name: "Book",
@@ -10,3 +10,20 @@ const BookType = GraphQLObjectType({
     genre: { type: GraqhQLString }
   })
 })
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  field: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraqhQLString } },
+      resolve(parent, args) {
+        // 从哪里得到数据，比如数据库或其他来源
+      }
+    }
+  }
+})
+
+module.exports = new GraqhQLSchema({
+  query: RootQuery
+});
