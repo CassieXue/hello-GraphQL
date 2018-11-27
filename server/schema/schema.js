6dcfc0d82c349f47881d58263c9fc67176a78f43
hfpp2012 <hfpp2012@gmail.com>
2018-11-27 13:06:02 +0800

    06_Resolve_Function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraqhQLString, GraqhQLSchema } = require('graphql');
+
+const books = [
+  { name: "算法导论", genre: "计算机科学", id: "1" },
+  { name: "人性的弱点", genre: "社交", id: "2" },
+  { name: "明朝那些事儿", genre: "历史", id: "3" }
+]
 
 const BookType = GraphQLObjectType({
   name: "Book",
@@ -19,6 +26,8 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraqhQLString } },
       resolve(parent, args) {
         // 从哪里得到数据，比如数据库或其他来源
+        // Mongodb mysql postgresql
+        _.find(books, { id: args.id })
       }
     }
   }
