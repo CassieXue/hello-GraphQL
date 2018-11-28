9fbb7da37bae5234b502a1f7bde383bb982ba68e
hfpp2012 <hfpp2012@gmail.com>
2018-11-28 08:31:02 +0800

    10_Author_Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,14 +5,21 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema,
-  GraphQLID
+  GraphQLID,
+  GraphQLInt
 } = require('graphql');
 
 const books = [
   { name: "算法导论", genre: "计算机科学", id: "1" },
   { name: "人性的弱点", genre: "社交", id: "2" },
   { name: "明朝那些事儿", genre: "历史", id: "3" }
-]
+];
+
+const authors = [
+  { name: "hfpp2012", age: 27, id: "1" },
+  { name: "rails365", age: 30, id: "2" },
+  { name: "lili", age: 21, id: "3" }
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -20,6 +27,15 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
+  })
+})
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
   })
 })
 
@@ -34,6 +50,13 @@ const RootQuery = new GraphQLObjectType({
         // Mongodb mysql postgresql
         console.log(typeof(args.id));
         return _.find(books, { id: args.id })
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id })
       }
     }
   }
