f67a2a825fca2a2a3056d08ec60c9353b87a6fac
hfpp2012 <hfpp2012@gmail.com>
2018-11-28 15:12:56 +0800

    14_mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// 连接到 mlab 数据库
+// 替换自己的用户名和密码
+mongoose.connect("mongodb://test:test123@ds131914.mlab.com:31914/graphql", { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
