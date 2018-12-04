be82eae1e6b17d85458efea80ff46c4f72c14d16
hfpp2012 <hfpp2012@gmail.com>
2018-12-04 10:52:52 +0800

    send query

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 // 连接到 mlab 数据库
 // 替换自己的用户名和密码
