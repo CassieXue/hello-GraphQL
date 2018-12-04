3d4b55db188de201deac7a29079a64c68cdea4b0
hfpp2012 <hfpp2012@gmail.com>
2018-12-04 16:16:23 +0800

    23_Rendering Data

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,13 +12,30 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends Component {
+  displayBooks = () => {
+    const data = this.props.data;
+    if (data.loading) {
+      return (<div>Loading books...</div>)
+    } else {
+      return (
+        <ul id="book-list">
+          {
+            data.books.map(book => {
+              return (
+                <li key={ book.id }>{ book.name }</li>
+              )
+            })
+          }
+        </ul>
+      )
+    }
+  }
+
   render() {
     console.log(this.props);
     return (
       <div>
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
+       { this.displayBooks() }
       </div>
     );
   }
